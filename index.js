dfb5710ec22000bcea55dd451b92103df72ae27f
i-muiri <ianmuiri99@gmail.com>
2022-11-18 05:31:40 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string){
+    return "hello".toUpperCase();
+}
+
+function whisper(string){
+    return "hello".toLowerCase();
+}
+
+function logShout(string){
+    console.log("hello".toUpperCase()); 
+}
+
+function logWhisper(string){
+    console.log("hello".toLowerCase()); 
+}
+
+function sayHiToHeadphonedRoommate(string){
+   
+    
+    if (string === "hello"){
+        return "I can\'t hear you!";
+    }
+        else if (string === "HELLO") {
+            return "YES INDEED!";
+        }
+
+        else if ("Let\'s have dinner together" === "Let\'s have dinner together") {
+            return "I would love to!";
+        }
+    }
+
+
